2d3852e47666256e5e6143088ead9229341ec949
PS4IDEA <76072886+PS4IDEA@users.noreply.github.com>
2025-02-18 10:58:50 -0800

    z

--- payload_map.js
+++ payload_map.js
@@ -3,6 +3,20 @@
 const CUSTOM_ACTION_APPCACHE_REMOVE = "appcache-remove";
 
 /**
+ * @typedef {Object} PayloadInfo
+ * @property {string} displayTitle
+ * @property {string} description
+ * @property {string} fileName - path relative to the payloads folder
+ * @property {string} author
+ * @property {string} projectSource
+ * @property {string} binarySource - should be direct download link to the included version, so that you can verify the hashes
+ * @property {string} version
+ * @property {string[]?} [supportedFirmwares] - optional, these are interpreted as prefixes, so "" would match all, and "4." would match 4.xx, if not set, the payload is assumed to be compatible with all firmwares
+ * @property {number?} [toPort] - optional, if the payload should be sent to "127.0.0.1:<port>" instead of loading directly, if specified it'll show up in webkit-only mode too
+ * @property {string?} [customAction]
+ */
+
+/**
  * @type {PayloadInfo[]}
 */
 const payload_map = [
@@ -22,6 +36,10 @@ const payload_map = [
         displayTitle: "etaHEN",
         description: "AIO HEN",
         fileName: "etaHEN.bin",
+        author: "LightningMods, Buzzer, sleirsgoevy, ChendoChap, astrelsky, illusion, CTN, SiSTR0, Nomadic",
+        projectSource: "https://github.com/LightningMods/etaHEN",
+        binarySource: "https://github.com/LightningMods/etaHEN/releases/download/1.9b/etaHEN.bin",
+        version: "1.9b",
         supportedFirmwares: ["3.", "4."]
     },
     {
@@ -68,6 +86,12 @@ const payload_map = [
     },
     {
         displayTitle: "websrv",
+        description: "Uses john-tornblom's elfldr. Custom homebrew loader. Runs on port 8080.",
+        fileName: "websrv.elf",
+        author: "john-tornblom",
+        projectSource: "https://github.com/ps5-payload-dev/websrv",
+        binarySource: "https://github.com/ps5-payload-dev/websrv/releases/tag/v0.18",
+        version: "0.18",
         toPort: 9021
     },
     {
