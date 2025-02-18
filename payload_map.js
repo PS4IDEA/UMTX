e0ef47eeebd3c54ed3d2ecc7f08e5dbddf8f35f8
PS4IDEA <76072886+PS4IDEA@users.noreply.github.com>
2025-02-18 11:42:58 -0800

    bbb

--- payload_map.js
+++ payload_map.js
@@ -39,7 +39,7 @@ const payload_map = [
         author: "LightningMods, Buzzer, sleirsgoevy, ChendoChap, astrelsky, illusion, CTN, SiSTR0, Nomadic",
         projectSource: "https://github.com/LightningMods/etaHEN",
         binarySource: "https://github.com/LightningMods/etaHEN/releases/download/1.9b/etaHEN.bin",
-        version: "2.0b",
+        version: "1.9b",
         supportedFirmwares: ["3.", "4.", "5."]
     },
     {
