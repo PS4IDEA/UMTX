07502328f504c21609b637da2cc17a98de8fc4f7
PS4IDEA <76072886+PS4IDEA@users.noreply.github.com>
2025-03-02 14:40:41 -0800

    00000

--- payload_map.js
+++ payload_map.js
@@ -53,6 +53,16 @@ const payload_map = [
         supportedFirmwares: ["1.", "2."],
         toPort: 9021
     },
+	{
+        displayTitle: "kstuff550",
+        description: "FPKG enabler",
+        fileName: "kstuff.elf",
+        author: "sleirsgoevy",
+        projectSource: "https://github.com/sleirsgoevy/ps4jb-payloads/tree/bd-jb/ps5-kstuff",
+        binarySource: "https://github.com/sleirsgoevy/ps4jb2/blob/3e6053c3e4c691a9ccdc409172293a81de00ad7f/ps5-kstuff.bin",
+        version: "3e6053c",
+        supportedFirmwares: ["5.50",]
+    },
     {
         displayTitle: "ps5-kstuff",
         description: "FPKG enabler",
