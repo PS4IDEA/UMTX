781eac6af4fde6e763f98168fcbeef6b8537acfc
PS4IDEA <76072886+PS4IDEA@users.noreply.github.com>
2025-03-08 06:18:54 -0800

    Add files via upload

--- payload_map.js
+++ payload_map.js
@@ -42,17 +42,6 @@ const payload_map = [
         version: "1.9b",
         supportedFirmwares: ["3.", "4."]
     },
-	 {
-        displayTitle: "etaHEN5xx",
-        description: "AIO HEN",
-        fileName: "etaHEN.bin",
-        author: "LightningMods, Buzzer, sleirsgoevy, ChendoChap, astrelsky, illusion, CTN, SiSTR0, Nomadic",
-        projectSource: "https://github.com/LightningMods/etaHEN",
-        binarySource: "https://github.com/LightningMods/etaHEN/releases/download/1.9b/etaHEN.bin",
-        version: "2.0b",
-        supportedFirmwares: ["5."],
-	toPort: 9021
-    },
     {
         displayTitle: "etaHEN",
         description: "AIO HEN",
@@ -68,8 +57,7 @@ const payload_map = [
         displayTitle: "kstuff550",
         description: "FPKG enabler",
         fileName: "kstuff.elf",
-        supportedFirmwares: ["5.50"],
-	toPort: 9021	
+        supportedFirmwares: ["5.50",]
     },
     {
         displayTitle: "ps5-kstuff",
